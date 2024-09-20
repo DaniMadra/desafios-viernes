2399a25ac3215dba89ad7cc25375cf4b0870dc45
Daniel <dmadrazoc@gmail.com>
2024-09-20 19:54:57 -0300

    NU

--- 2.1/assets/main.js
+++ 2.1/assets/main.js
@@ -1,3 +1,21 @@
-alert("hola desde la del papu")
-console.log("hola")
+
+
 console.error('error')
+
+let X = 10
+const Y = 5 
+
+
+
+let nombre = "MADRA"
+
+X = 16
+
+console.log(X-Y)
+
+console.log("tu nombre es: " + nombre + "Y tu edad es: " + X)
+console.log(`tu nombre es: ${nombre} y tu edad es: ${X}`)
+
+const NU = prompt('Ingrese su nombre:')
+
+console.log(NU)
