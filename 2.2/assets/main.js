a2da958f1663bd5423ffa4b685e66cda9bc67a28
Daniel <dmadrazoc@gmail.com>
2024-09-27 17:00:10 -0300

    Codigo de Compra

---
+++ 2.2/assets/main.js
@@ -0,0 +1,12 @@
+
+let nombreProducto = "Billetera";
+let precioUnidad = 28000; 
+
+
+let cantidadDeseada = prompt("¿Cuántas unidades de " + nombreProducto + " quiere comprar?");
+
+
+let costoTotal = precioUnidad * cantidadDeseada;
+
+
+alert("El costo total de la compra es: $" + costoTotal);
