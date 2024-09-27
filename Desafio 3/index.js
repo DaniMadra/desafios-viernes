5a2cc1aef178ce2d055ec3d015e66ea1ce0f61f3
Daniel <dmadrazoc@gmail.com>
2024-09-27 19:53:54 -0300

    DESAFIO 3

--- Desafio 3/index.js
+++ Desafio 3/index.js
@@ -1,6 +1,6 @@
 
 const nombreProducto = "Billetera";
-let precioUnidad = 28000; 
+let precioUnidad = 280; 
 
 
 let cantidadDeseada =parseInt(prompt("¿Cuántas unidades de " + nombreProducto + " quiere comprar? \n Precio por unidad = $" + precioUnidad));
@@ -8,6 +8,10 @@ let cantidadDeseada =parseInt(prompt("¿Cuántas unidades de " + nombreProducto 
 
 let costoTotal = precioUnidad * cantidadDeseada;
 
+let costoConDescuento = precioUnidad * cantidadDeseada - precioUnidad * 1/10
 
+if(cantidadDeseada < 5)
 alert("El costo total de la compra es: $" + costoTotal);
 
+if(cantidadDeseada >= 5)
+    alert("Usted a recibido un descuento del 10% al comprar mas de 5 unidades \n Precio con descuento = $" + costoConDescuento)
